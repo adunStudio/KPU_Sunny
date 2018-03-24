d3dd7926acea44671b580032731281fc88299df6
GodHongIl <adunstudio@daum.net>
2018-03-24 21:16:23 +0900

    TRS Matrix 분리 테스트

--- Sunny-Core/maths/mat4.cpp
+++ Sunny-Core/maths/mat4.cpp
@@ -383,9 +383,9 @@ namespace sunny
         std::string mat4::ToString() const
         {
             std::stringstream result;
-            result << "mat4: (" << rows[0].x << ", " << rows[1].x << ", " << rows[2].x << ", " << rows[3].x << "), ";
-            result << "(" << rows[0].y << ", " << rows[1].y << ", " << rows[2].y << ", " << rows[3].y << "), ";
-            result << "(" << rows[0].z << ", " << rows[1].z << ", " << rows[2].z << ", " << rows[3].z << "), ";
+            result << "mat4: \n(" << rows[0].x << ", " << rows[1].x << ", " << rows[2].x << ", " << rows[3].x << "),\n";
+            result << "(" << rows[0].y << ", " << rows[1].y << ", " << rows[2].y << ", " << rows[3].y << "),\n";
+            result << "(" << rows[0].z << ", " << rows[1].z << ", " << rows[2].z << ", " << rows[3].z << "),\n";
             result << "(" << rows[0].w << ", " << rows[1].w << ", " << rows[2].w << ", " << rows[3].w << ")";
             return result.str();
         }
